05e67d583985f289cf5c3ffe9b9d149418a7a2cc
Leonhest <leon.hesthaug@gmail.com>
2021-07-04 21:03:42 +0200

    Array function

---
+++ minesweeper.py
@@ -0,0 +1,12 @@
+#Start of minesweeper code
+def minesweeper(n):
+    arr = [[0 for row in range(n)] for column in range(n)]
+    for row in arr:
+        print(" ".join(str(cell) for cell in row))
+        print("")
+
+
+grid_size = input("Size of grid")
+
+minesweeper(int(grid_size))
+
